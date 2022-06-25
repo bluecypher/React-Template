ccdb19f0c504db10bbf3cfff599da553803c55ae
shahi <s.sandeepshahi@gmail.com>
2022-06-25 16:12:47 +0530

    template reactJS

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,83 @@
+/*eslint-disable*/
+import { Flex, Link, List, ListItem, Text } from "@chakra-ui/react";
+import React from "react";
+
+export default function Footer(props) {
+  return (
+    <Flex
+      flexDirection={{
+        base: "column",
+        xl: "row",
+      }}
+      alignItems={{
+        base: "center",
+        xl: "start",
+      }}
+      justifyContent='space-between'
+      px='30px'
+      pb='20px'>
+      <Text
+        color='gray.400'
+        textAlign={{
+          base: "center",
+          xl: "start",
+        }}
+        mb={{ base: "20px", xl: "0px" }}>
+        &copy; {1900 + new Date().getYear()},{" "}
+        <Text as='span'>
+          {"Made with ❤️ by "}
+        </Text>
+        <Link
+          color='blue.400'
+          href='Sahayaks'
+          target='_blank'>
+          {"Sahayaks "}
+        </Link>
+        &
+        <Link color='blue.400' href='https://www.simmmple.com' target='_blank'>
+          {" Simmmple"}
+        </Link>
+        {" for a better web"}
+      </Text>
+      <List display='flex'>
+        <ListItem
+          me={{
+            base: "20px",
+            md: "44px",
+          }}>
+          <Link color='gray.400' href='https://www.creative-tim.com'>
+            {"Creative Tim"}
+          </Link>
+        </ListItem>
+        <ListItem
+          me={{
+            base: "20px",
+            md: "44px",
+          }}>
+          <Link color='gray.400' href='https://www.simmmple.com'>
+            {"Simmmple"}
+          </Link>
+        </ListItem>
+        <ListItem
+          me={{
+            base: "20px",
+            md: "44px",
+          }}>
+          <Link
+            color='gray.400'
+
+            href='https://sahayaks.com'>
+            {"Blog"}
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link
+            color='gray.400'
+            href='https://sahayaks.com'>
+            {"License"}
+          </Link>
+        </ListItem>
+      </List>
+    </Flex>
+  );
+}
